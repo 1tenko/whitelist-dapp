093d38676e4527b25fc265cb25a885a9e2e8bea7
tenko <tajm.ogunbiyi@gmail.com>
2022-07-10 21:20:59 +0100

    :construction_worker: Deployed Whitelist.sol

--- hardhat-tutorial/scripts/deploy.ts
+++ hardhat-tutorial/scripts/deploy.ts
@@ -1,18 +1,22 @@
 import { ethers } from "hardhat";
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-  const unlockTime = currentTimestampInSeconds + ONE_YEAR_IN_SECS;
+  // a ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  //so whitelistContract here is a factory for instances of our Whitelist contract
+  const whitelistContract = await ethers.getContractFactory("Whitelist");
 
-  const lockedAmount = ethers.utils.parseEther("1");
+  // here we deploy the contract
+  const deployedWhitelistContract = await whitelistContract.deploy(10);
+  // 10 is the maximum number of whitelisted addresses allowed
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // wait for the contract to finish deploying
+  await deployedWhitelistContract.deployed();
 
-  await lock.deployed();
-
-  console.log("Lock with 1 ETH deployed to:", lock.address);
+  // print the address of the deployed contract
+  console.log(
+    "Whitelist Contract Address: ",
+    deployedWhitelistContract.address
+  );
 }
 
 // We recommend this pattern to be able to use async/await everywhere
